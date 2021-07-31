1b934e6c414c06e6f222fb6671ea72f3cc454c63
kwong25 <kwong25@u.rochester.edu>
2021-07-31 13:18:52 -0400

    added current page menu highlighter

--- inc/bottom.php
+++ inc/bottom.php
@@ -3,5 +3,9 @@
         </div>
         </div>
     
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <script src="js/menu-highlighter.js"></script>
+
     </body>
 </html>
